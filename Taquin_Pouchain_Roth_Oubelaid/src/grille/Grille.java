dd339aaab24240509810604c96ab5177cc6c26bf
nariane <narianekatpika@gmail.com>
2020-10-02 09:13:04 +0200

    Classe Case et Grille ( la méthode fin de partie pas faite)

--- Taquin_Pouchain_Roth_Oubelaid/src/grille/Grille.java
+++ Taquin_Pouchain_Roth_Oubelaid/src/grille/Grille.java
@@ -10,5 +10,36 @@ package grille;
  * @author Perso
  */
 public class Grille {
+    private Case[][] grilleCase ;
+    private String image ;
+    private int taille;
+    
+    public void Grille(int uneTaille){
+        this.taille =  uneTaille; 
+    }
+    
+    public Case[][] getGrille(){
+        return grilleCase;
+    }
+    
+    public void setImage(String chemin){
+        this.image = chemin; 
+    }
+    
+    @Override
+    public String toString(){
+        String text ="[";
+        for (int i = 0; i < this.grilleCase.length; i++) {
+            for (int j = 0; j < this.grilleCase[i].length; j++) {
+                text += " " + this.grilleCase[i][j].getValue() + " |";
+            }
+            text += '\n';
+        }
+        text += "]";
+        return text; 
+    }
+    
+
+    
     
 }
