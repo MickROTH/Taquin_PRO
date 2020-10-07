abe137d8decd7795f3cdde3d317b0b5e308cac6c
nariane <narianekatpika@gmail.com>
2020-10-07 09:38:20 +0200

    Commentaire et préparation pour la javadoc

--- Taquin_Pouchain_Roth_Oubelaid/src/grille/Grille.java
+++ Taquin_Pouchain_Roth_Oubelaid/src/grille/Grille.java
@@ -10,15 +10,23 @@ package grille;
  * @author Perso
  */
 public class Grille {
+    //Attributs de la classe
     private Case[][] grilleCase ;
     private String image ;
     private int taille;
     
 
-    
+    /**
+     * constructeur de la classe 
+     * @param uneTaille la taille hauteur et largeur de la grille 
+     * @param estObjectif qui nous dit si la grille est une grille objectif ou une grille de jeu 
+     * La grille objectif est ranger dans l'ordre 
+     * La grille non objectif est dans le désodre comme au démarage d'une partie 
+     */
     public void Grille(int uneTaille, boolean estObjectif){
         this.grilleCase =  new Case[uneTaille][uneTaille];
         int compteur =0; 
+        //valuer les case de 0 à  taille*taille-1
          for (int i = 0; i < this.grilleCase.length; i++) {
             for (int j = 0; j < this.grilleCase[i].length; j++) {
                 this.grilleCase[i][j].setValue(compteur);
@@ -26,8 +34,9 @@ public class Grille {
             }
         }
          
-        // si le tableau est pas tableau objesctif il est pas mélanger 
+        // si le tableau est pas tableau objectif il est pas mélanger 
         if (!estObjectif) {
+            //on fait 1000 permutation des éléments 
             for (int i = 0; i < 1000; i++) {
                 Case temp = new Case();
                 int random1 = 0 + (int)(Math.random() * ((this.grilleCase.length - 0) + 1));
@@ -39,18 +48,34 @@ public class Grille {
                 this.grilleCase[random3][random4].setValue(temp.getValue()) ;                
             }
         }
-         
         this.taille =  uneTaille; 
     }
-
+    
+    
+    /**
+     * Permet de récuperer la grille 
+     * @return 
+     */
     public Case[][] getGrille(){
         return grilleCase;
     }
     
+    /**
+     * Permet de modifier l'image de la grille 
+     * @param chemin 
+     */
     public void setImage(String chemin){
         this.image = chemin; 
     }
     
+    
+    /**
+     * Affichage de la grille sous la forme 
+     * [ 0 | 1 | 2 |
+     *  3 | 4 | 5 |
+     *  6 | 7 | 8]
+     * @return 
+     */
     @Override
     public String toString(){
         String text ="[";
@@ -64,8 +89,11 @@ public class Grille {
         return text; 
     }
     
-
-    // cette méthode servira principalement a comparer si la grille courante, à la grille objectif ( voir si la partie est fini)
+    /**
+     * cette méthode servira principalement a comparer si la grille courante, à la grille objectif ( voir si la partie est fini)
+     * @param autreGrille
+     * @return 
+     */
     public boolean estPareil(Grille autreGrille){
         boolean bool= true ;
         for (int i = 0; i < this.grilleCase.length; i++) {
