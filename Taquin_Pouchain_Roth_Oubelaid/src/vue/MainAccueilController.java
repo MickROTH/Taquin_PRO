97c4003eea30a2502196b157a4905d7ed621d955
nariane <narianekatpika@gmail.com>
2020-11-06 12:46:18 +0100

    Changer l'image d'un imageview changer de fenetre (accueil -> parametre de partie) Look et design de la fenetre parametre

--- Taquin_Pouchain_Roth_Oubelaid/src/vue/MainAccueilController.java
+++ Taquin_Pouchain_Roth_Oubelaid/src/vue/MainAccueilController.java
@@ -7,11 +7,13 @@ package vue;
 
 import Console.JeuSoloConsole;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,6 +22,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -34,12 +38,15 @@ public class MainAccueilController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
 
     }
+    
+    @FXML
+    ImageView imageFond;
 
     @FXML
     private void buttAvecSoloOnClick(ActionEvent event) throws IOException {
 
-        //OURVRIR QQCHOSE EN MODE GRAPHIQUE 
-        Parent root = FXMLLoader.load(getClass().getResource("MainAccueil.fxml"));
+        //Ouvrir un autre fenetre graphisue 
+        Parent root = FXMLLoader.load(getClass().getResource("MainGraphiqueGame.fxml"));
 
         Scene scene = new Scene(root);
 
@@ -47,18 +54,56 @@ public class MainAccueilController implements Initializable {
 
         gameStage.setScene(scene);
         gameStage.show();
+        
+        /*
+        OU 
+        
+        Stage stageTheLabelBelongs = (Stage) label.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/logine.fxml"));
+        Scene scene = new Scene(root);
+        stageTheLabelBelongs.setScene(scene);
+        stageTheLabelBelongs.show();
+        
+*/
+
+/* 
+CHANGER UNE IMAGE 
+  System.out.println("je suis un bouton ");
+        Image image = new Image("https://external-preview.redd.it/GOkP8onbuyjGmN9Rc8Que5mw21CdSw6OuXpAKUuE6-4.jpg?auto=webp&s=2bc0e522d1f2fa887333286d557466b2be00fa5e");
+         Class<?> clazz = this.getClass();
+        try{
+            
+        InputStream input = clazz.getResourceAsStream("/images/jetest.jpg");
+ 
+        Image image1 = new Image(input);
+ 
+        imageFond.setImage(image1);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+*/
+      
+        
+ 
+        System.out.println("tu m'as cliquer dessus ");
 
     }
 
     @FXML
     private void buttSansSoloOnClick(ActionEvent event) throws IOException {
         System.out.println("Oh on a cliquer sur moi !");
-
+        
+        // il faut cacher la fenetre d'accueil 
+        
+        
+        
         JeuSoloConsole jeu = new JeuSoloConsole();
         String[] arg = new String[10];
         JeuSoloConsole.main(arg);
 
-        // il faut cacher la fenetre d'accueil 
+        
     }
 
 }
