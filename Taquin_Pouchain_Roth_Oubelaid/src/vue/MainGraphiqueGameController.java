a721d10c0d7c7de09c66354ae2fc36ac8ed2a1ee
nariane <narianekatpika@gmail.com>
2020-11-09 11:18:27 +0100

    Mise ne lien entre la partie de Sunny et mes fenetres Mise en commun du formalisme des noms de classes

--- Taquin_Pouchain_Roth_Oubelaid/src/vue/MainGraphiqueGameController.java
+++ Taquin_Pouchain_Roth_Oubelaid/src/vue/MainGraphiqueGameController.java
@@ -8,20 +8,74 @@ package vue;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
-/**
- * FXML Controller class
- *
- * @author Perso
- */
 public class MainGraphiqueGameController implements Initializable {
+    /* 
+    *Variables globales correspondant à des objets définis dans la vue (fichier .fxml)
+    *Ces variables sont ajoutées à la main et portent le même nom que les fx:id dans Scene Builder
+    */
+    /*@FXML
+    private BorderPane Fond;
+    @FXML
+    private Label Score; 
+    @FXML
+    private Label Deplacement;
+    @FXML
+    private Button Play;
+    @FXML
+    private Button Look;
+    @FXML
+    private Button Image;
+    @FXML
+    private TextField Taille;
+    @FXML
+    private GridPane Grille;
+    @FXML
+    private Menu Classement;
+    @FXML
+    private Menu Aide;
+    @FXML
+    private MenuItem Sauvegarde;
+    @FXML
+    private MenuItem Charge;
+    @FXML
+    private MenuItem Quitter;
+    */
+    @FXML
+    private ImageView buttonPlay;
+    //@FXML
+    //private ImageView imageGrille;
 
+    private boolean etatBouton;
+    
+    @FXML
+    public void handleButtonAction(){ //methode pour qu'au clique l'image du bouton se change  en "pause" et inversement en "play"
+        if (true==etatBouton){
+            buttonPlay.setImage(new Image("images/pause.png")); 
+        }if(false==etatBouton){
+            buttonPlay.setImage(new Image("images/play.png"));
+        }
+        etatBouton=!etatBouton;
+   
+}
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        etatBouton=true;
+    }
     }    
     
-}
+
+
